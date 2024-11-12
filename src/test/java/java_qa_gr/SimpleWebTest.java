14f58bbe63ffab2f344bf82642223043395327fd
sem1on <sgorlov21@gmail.com>
2024-11-12 21:55:09 +0300

    add SimpleWebTest

--- src/test/java/java_qa_gr/SimpleWebTest.java
+++ src/test/java/java_qa_gr/SimpleWebTest.java
@@ -1,4 +1,19 @@
 package java_qa_gr;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
+
 public class SimpleWebTest {
+
+    @Test
+    @DisplayName("Проверка числа результатов")
+    void serchResultItems10() {
+        open("https://ya.ru");
+        $("#text").setValue("selenide");
+        $("button[type='submit']").click();
+        $$("li.serp-item").shouldHave(sizeGreaterThanOrEqual(10));
+    }
 }
