30eb443e9430f2978ce933c32178860553f87d67
sem1on <sgorlov21@gmail.com>
2024-11-13 22:37:26 +0300

    add CsvSource

--- src/test/java/java_qa_gr/SimpleWebTest.java
+++ src/test/java/java_qa_gr/SimpleWebTest.java
@@ -3,7 +3,11 @@ package java_qa_gr;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
 
@@ -14,12 +18,27 @@ public class SimpleWebTest {
         open("https://ya.ru");
     }
 
-    @Test
-    @DisplayName("Проверка числа результатов")
+    @ValueSource(strings = {
+            "seleide", "allure"
+    })
+    @ParameterizedTest(name = "Проверка числа результатов {0}")
     @Blocker
-    void serchResultItems10() {
-        $("#text").setValue("selenide");
+    void searchResultItems10(String testData) {
+        $("#text").setValue(testData);
         $("button[type='submit']").click();
         $$("li.serp-item").shouldHave(sizeGreaterThanOrEqual(10));
+    }
+
+    @CsvSource(value = {
+            "seleide|   стабильные UI тесты",
+            "allure|   AllureFramework GitHub"
+    },
+    delimiter = '|')
+    @ParameterizedTest(name = "В первом результате для {0} текст {1}")
+    @Blocker
+    void FirstSearchResultItems(String testData, String expecktResult) {
+        $("#text").setValue(testData);
+        $("button[type='submit']").click();
+        $$("li.serp-item").first().shouldHave(text(expecktResult));
     }
 }
