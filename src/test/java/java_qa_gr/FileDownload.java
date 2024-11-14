dbd1e5ead909523310185de61ae7aeb724b63bad
sem1on <sgorlov21@gmail.com>
2024-11-14 22:13:01 +0300

    add Read file

--- src/test/java/java_qa_gr/FileDownload.java
+++ src/test/java/java_qa_gr/FileDownload.java
@@ -1,8 +1,12 @@
 package java_qa_gr;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -13,5 +17,11 @@ public class FileDownload {
     void downLoadTest() throws Exception {
         open("https://github.com/sem1on/google-clone/blob/main/README.md");
         File file = $("[data-testid=raw-button]").download();
+
+        try (InputStream is = new FileInputStream(file)) {
+            byte[] bytes = is.readAllBytes();
+            String fileToString = new String(bytes, StandardCharsets.UTF_8);
+            Assertions.assertTrue(fileToString.contains("Google Search API"));
+        }
     }
 }
